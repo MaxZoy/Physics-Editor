7de0c4ad0fb8f18d4c0c7b7a8df19a29b698a6d0
MaxZoy <106869937+MaxZoy@users.noreply.github.com>
2026-07-18 19:33:14 +0800

    Delete Scripts directory

--- Scripts/Editor/AboutWindows.gd
+++
@@ -1,6 +0,0 @@
-extends Window
-
-@onready var version_label: Label = $Panel/VBoxContainer/VBoxContainer/Version
-
-func _ready():
-	version_label.text = "版本：" + ProjectSettings.get_setting("application/config/version")
